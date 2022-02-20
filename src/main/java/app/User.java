93977a4d3b07a094c835d3ba8deed3b4508a1035
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-02-20 08:11:55 +0000

    Create User Model

---
+++ src/main/java/app/User.java
@@ -0,0 +1,108 @@
+package app;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String first_name;
+
+    @NotBlank
+    private String surname;
+
+    @NotBlank
+    private String date_of_birth;
+
+    @NotBlank
+    private String phone_number;
+
+    @NotBlank
+    private String email_address;
+
+    @NotBlank
+    private String nationality;
+
+    @NotBlank
+    private String ppsn;
+
+    @NotBlank
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPpsn() {
+        return ppsn;
+    }
+
+    public void setPpsn(String ppsn) {
+        this.ppsn = ppsn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
