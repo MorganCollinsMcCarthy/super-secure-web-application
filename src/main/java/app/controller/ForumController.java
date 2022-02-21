cd3d5184c3578831cffb0a79ce63901fadf0f302
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-02-21 15:25:36 +0000

    Created forum controller

--- src/main/java/app/controller/ForumController.java
+++ src/main/java/app/controller/ForumController.java
@@ -1,16 +1,13 @@
 package app.controller;
 
+import app.persistence.model.Post;
 import app.persistence.repository.PostRepository;
 import app.service.IUserService;
-import app.persistence.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 
 @Controller
 public class ForumController {
@@ -19,11 +16,23 @@ public class ForumController {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private IUserService userService;
+
     @GetMapping("/forum")
     public String showForum(Model model) {
         System.out.println(postRepository.findAll());
         model.addAttribute("posts",postRepository.findAll());
         return "forum.html";
+    }
+
+    @PostMapping("/forum/add")
+    public @ResponseBody
+    Post addPost(@RequestBody String content) {
+        Post post = new Post();
+        post.setContent(content);
+        post.setUser(userService.getAuthenticatedUser());
+        return postRepository.save(post);
     }
 }
 
