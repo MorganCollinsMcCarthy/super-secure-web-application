b860abed6b69252867ac59b34c541f712ceb35a4
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-02-26 22:08:34 +0000

    Registration model and view

--- src/main/java/app/controller/RegistrationController.java
+++ src/main/java/app/controller/RegistrationController.java
@@ -1,14 +1,18 @@
 package app.controller;
 
+import app.exception.UserAlreadyExistException;
 import app.service.IUserService;
 import app.persistence.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -26,8 +30,17 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String registerUserAccount(@ModelAttribute("user") @Valid User user) {
-        userService.registerNewUserAccount(user);
-        return null;
+    public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid final User user, final HttpServletRequest request, final Errors errors) {
+
+        try {
+            final User registered = userService.registerNewUserAccount(user);
+        } catch (final UserAlreadyExistException uaeEx) {
+            ModelAndView mav = new ModelAndView("registration", "user", user);
+            mav.addObject("error", "An account for that username/email already exists.");
+            return mav;
+        }
+        ModelAndView mav = new ModelAndView("registration", "user", user);
+        mav.addObject("success", "You registered successfully.");
+        return mav;
     }
 }
