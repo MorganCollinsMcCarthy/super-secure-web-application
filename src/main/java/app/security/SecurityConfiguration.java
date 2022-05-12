39386257c7f76e10b813d861a667320e157986f0
MorganCollinsMcCarthy <morganmainmail@gmail.com>
2022-05-12 23:34:22 +0100

    Changing security to custom auth

--- src/main/java/app/security/SecurityConfiguration.java
+++ src/main/java/app/security/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package app.security;
 
+import app.security.google2fa.CustomAuthenticationProvider;
+import app.security.google2fa.CustomWebAuthenticationDetailsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,10 +19,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+    @Autowired
+    private CustomWebAuthenticationDetailsSource authenticationDetailsSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,11 +38,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .requiresChannel().antMatchers("/**").requiresSecure()
 
                 .and()
-                .formLogin().loginPage("/login").permitAll();
+                .formLogin()
+                .loginPage("/login")
+                .authenticationDetailsSource(authenticationDetailsSource)
+                .permitAll();
     }
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        CustomAuthenticationProvider authProvider = new CustomAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
     }
 }
